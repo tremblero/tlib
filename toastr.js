529ed60b6e1a525199154cc98f94cc28d5279245
Trembling <103013062+tremblero@users.noreply.github.com>
2023-06-19 15:18:15 -0400

    Create toastr.js

---
+++ toastr.js
@@ -0,0 +1,9 @@
+// appends toastr
+
+var css = document.createElement('link');
+css.href = 'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css';
+css.rel = 'stylesheet';
+document.head.appendChild(css);
+var s = document.createElement('script')
+s.src='https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js'
+document.head.appendChild(s);
