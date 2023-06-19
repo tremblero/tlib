d4aa86ae1dbacbba4861a6a7955981d9918df3e5
Trembling <103013062+tremblero@users.noreply.github.com>
2023-06-19 15:32:29 -0400

    Update toastr.js

--- toastr.js
+++ toastr.js
@@ -1,14 +1,13 @@
-// appends toastr
-
 var css = document.createElement('link');
 css.href = 'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css';
 css.rel = 'stylesheet';
 document.head.appendChild(css);
-var s = document.createElement('script')
-s.src = 'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js'
-document.head.appendChild(s);
-let toast
 
-s.onload(() => {
-    toast = toastr
-})
+var s = document.createElement('script');
+s.src = 'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js';
+
+s.addEventListener('load', function() {
+    toast = toastr;
+});
+
+document.head.appendChild(s);
